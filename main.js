aecf1f31c1c7898700ae7427da06e7758fdf74be
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2020-11-27 22:33:22 +0530

    new stuff

--- main.js
+++ main.js
@@ -4,12 +4,139 @@ console.log(
   "background: #222; color: #bada55"
 );
 //program
+
+//normal
 function think(statement) {
   console.log(statement);
 }
 
 function message(content) {
   alert(content);
+}
+
+function question(content) {
+  prompt(content);
+}
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+}
+
+//arrays
+
+function joinArray(array1, array2) {
+  return array1.concat(array2);
+}
+
+function removeLastElement(array) {
+  array.pop();
+}
+
+function addElement(array, content) {
+  array.push(content);
+}
+
+function reverse(array) {
+  array.reverse();
+}
+
+function removeFirstElement(array) {
+  array.shift();
+}
+
+function addNewFirstElement(array) {
+  array.unshift();
+}
+//geolocation
+
+function postion() {
+  getCurrentPosition();
+}
+
+function getPositionLatitude() {
+  return position.coords.latitude;
+}
+
+function getPositionLongitude() {
+  return position.coords.longitude;
+}
+//date
+
+function getYear() {
+  var d = new Date();
+  return d.getFullYear();
+}
+function getMonth() {
+  var d = new Date();
+  return d.getMonth();
+}
+
+function getWeekday() {
+  var d = new Date();
+  return d.getDay();
+}
+
+function getMonthDate() {
+  var d = new Date();
+  return d.getDate();
+}
+
+//math
+
+function minNum(x, y, z) {
+  return min(x, y, z);
+}
+
+function maxNum(x, y, z) {
+  return max(x, y, z);
+}
+
+function cubeRoot(num) {
+  return cbrt(num);
+}
+
+function absolute(num) {
+  return abs(num);
+}
+
+function EulerNum() {
+  return Math.E;
+}
+
+function PiNum() {
+  return Math.PI;
+}
+
+function root(num) {
+  return Math.sqrt(num);
 }
 
 function add(x, y) {
@@ -54,9 +181,7 @@ function equal(num1, num2) {
   }
 }
 
-function question(content) {
-  prompt(content);
-}
+//jquery
 
 function say(id, content) {
   $(id).text(content);
@@ -107,38 +232,7 @@ function keycode() {
   });
 }
 
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("include");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-            elmnt.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            elmnt.innerHTML = "Page not found.";
-          }
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("include");
-          includeHTML();
-        }
-      };
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
-}
-
+//lodash
 function repeat(times, func) {
   _.times(times, func);
 }
